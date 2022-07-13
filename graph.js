a4de052ea137bcfa3ef5eeaf960c0c6009e42ab0
AlexJanbo <Ajanbo@uci.edu>
2022-07-13 11:32:24 -0700

    main

--- graph.js
+++ graph.js
@@ -44,12 +44,12 @@ const bfsPrint = (graph, source) => {
 // bfsPrint(graph, 'a')
 
 
-const hasPath = (graph, source, destination) => {
+const hasPathDFS = (graph, source, destination) => {
     
     if(source === destination) return true
 
     for(let neighbor of graph[source]) {
-        if(hasPath(graph, neighbor, destination) === true) 
+        if(hasPathDFS(graph, neighbor, destination) === true) 
         return true
     }
 
@@ -69,3 +69,57 @@ const hasPathBFS = (graph, source, destination) => {
     }
     return false
 }
+
+const graphTwo = {
+    i: ['j', 'k'],
+    j: ['i'],
+    k: ['i', 'm', 'l'],
+    m: ['k'],
+    l: ['k'],
+    o: ['n'],
+    n: ['o']
+}
+
+const edges = [
+    ["i", 'j'],
+    ["k", "i"],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n']
+]
+
+const undirectedPath = (edges, nodeA, nodeB) => {
+    const graph = buildGraph(edges)
+    return hasPath(graph, nodeA, nodeB, new Set())
+}
+
+const hasPath = (graph, source, destination, visited) => {
+    if(source === destination) return true
+    if(visited.has(source)) return false
+
+    visited.add(source)
+
+    for(let neighbor of graph[source]) {
+        if(hasPath(graph, neighbor, destination, visited) === true) {
+            return true
+        }
+    }
+
+    return false
+}
+
+const buildGraph = (edges) => {
+    const graph = {}
+
+    for(let edge of edges) {
+        const [ a, b ] = edge
+        if(!(a in graph)) graph[a] = [];
+        if(!(b in graph)) graph[b] = [];
+        graph[a].push(b)
+        graph[b].push(a)
+    }
+
+    return graph
+}
+
+console.log(undirectedPath(edges, 'j', 'm'))
