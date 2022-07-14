915b7b214903eaa267d817f0cd278a539f7fd17e
AlexJanbo <Ajanbo@uci.edu>
2022-07-14 14:21:17 -0700

    find size of minimum island as a graph with dfs

--- graph.js
+++ graph.js
@@ -213,3 +213,78 @@ const makeGraph = (edges) => {
         graph[b].push(a)
     }
 }   
+
+const islandCount = (grid) => {
+    
+    const visited = new Set()
+    let count = 0
+    
+    for(let r = 0; r < grid.length; r++) {
+        for(let c = 0; grid[0].length; c++) {
+            if(exploreIslands(grid, r, c, visited) === true) {
+                count++
+            }
+        }
+    }
+
+    return count
+}
+
+const exploreIslands = (grid, r, c, visited) => {
+    
+    const rowInBounds = 0 <= r && r < grid.length
+    const colInBounds = 0 <= c && c < grid[0].length
+    if(!rowInBounds || !colInBounds) return false
+
+    if(grid[r][c] === 'w') return false
+
+    const position = r + ',' + c
+
+    if(visited.has(pos)) return false
+    visited.add(pos)
+
+    exploreIslands(grid, r-1, c, visited)
+    exploreIslands(grid, r+1, c, visited)
+    exploreIslands(grid, r, c-1, visited)
+    exploreIslands(grid, r, c+1, visited)
+
+    return true
+}
+
+
+const islandMinimum = (grid) => {
+    
+    const visited = new Set()
+    let minSize = Infinity
+
+    for(let r = 0; r < grid.length; r++) {
+        for(let c = 0; c < grid[0].length; c++){
+            const size = size + exploreIsland(grid, r, c, visited )
+            if(size > 0 && size < minSize) minSize = size
+        }
+    }
+}
+
+const exploreIsland = (grid, r, c, visited) => {
+
+    const rowInBounds = 0 <= r && r < grid.length
+    const colInBounds = 0 <= c && c < grid.length
+    if(!rowInBounds || colInBounds) return 0
+
+    if(grid[r][c] === "w") return 0
+
+    const position = r + ',' + c
+
+    if(visited.has(position)) return 0
+
+    visited.add(position)
+    
+    let size = 1;
+
+    size += exploreIsland(grid, r+1, c, visited)
+    size += exploreIsland(grid, r-1, c, visited)
+    size += exploreIsland(grid, r, c+1, visited)
+    size += exploreIsland(grid, r, c-1, visited)
+
+    return size
+}
