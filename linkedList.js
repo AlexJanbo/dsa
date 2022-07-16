58f3bb8a7ce40c9d44e373ac6bab1fa77dc8e9ea
AlexJanbo <Ajanbo@uci.edu>
2022-07-15 21:06:53 -0700

    Linked List Setup

---
+++ linkedList.js
@@ -0,0 +1,13 @@
+class node {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+class linkedList {
+    constructor(head = null) {
+        this.head = head
+        this.size = 0
+    }
+}
