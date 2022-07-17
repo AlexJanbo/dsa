2d66c1887bdec67916cad2efc7725b3463e0ec23
AlexJanbo <Ajanbo@uci.edu>
2022-07-17 14:23:44 -0700

    Linked List Traversal iterative and recursive

--- linkedList.js
+++ linkedList.js
@@ -1,13 +1,71 @@
-class node {
+class Node {
     constructor(data) {
         this.data = data
         this.next = null
     }
 }
 
-class linkedList {
-    constructor(head = null) {
+class LinkedList {
+    constructor( head = null ) {
         this.head = head
-        this.size = 0
     }
 }
+
+const addNode = (data) => {
+    let current = new Node(data)
+}
+
+let ll = new LinkedList()
+
+const a = new Node("A")
+const b = new Node("B")
+const c = new Node("C")
+const d = new Node("D")
+
+a.next = b
+b.next = c
+c.next = d
+
+const printLinkedList = (head) => {
+    let current = head
+    while(current !== null) {
+        console.log(current.data)
+        current = current.next
+    }
+}
+
+// printLinkedList(a)
+const printLinkedListRecurisvely = (head) => {
+    if(head === null) return
+
+    console.log(head.data)
+    printLinkedListRecurisvely(head.next)
+}
+
+// printLinkedListRecurisvely(a)
+
+const LinkedListValues = (head) => {
+    let current = head
+    let valuesArray = []
+    while(current !== null) {
+        valuesArray.push(current.data)
+        current = current.next
+    }
+    return valuesArray
+}
+
+// console.log(LinkedListValues(a))
+
+const LinkedListValuesRecursive = (head) => {
+    const valuesArray = []
+    fillValues(head, valuesArray)
+    return valuesArray
+}
+
+const fillValues = (head, valuesArray) => {
+    if(head === null) return
+    valuesArray.push(head.data)
+    fillValues(head.next, valuesArray)
+}
+
+console.log(LinkedListValuesRecursive(a))
