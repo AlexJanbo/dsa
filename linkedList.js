4b46514b65b6c23b51935a91491c9202641424db
AlexJanbo <Ajanbo@uci.edu>
2022-07-18 22:54:15 -0700

    Merge two sorted lists

--- linkedList.js
+++ linkedList.js
@@ -175,3 +175,32 @@ const ZipperList = (HeadOne, HeadTwo) => {
 
     return headOne 
 }
+
+var mergeTwoLists = function(list1, list2) {
+    
+    let dummy = new ListNode()
+    let tail = dummy
+    
+    let current1 = list1
+    let current2 = list2
+    
+    while(current1 !== null && current2 !== null) {
+        if(current1.val < current2.val) {
+            tail.next = current1
+            current1 = current1.next
+        } else {
+            tail.next = current2
+            current2 = current2.next
+        }
+        tail = tail.next
+    }
+    
+    if(current1 !== null) {
+        tail.next = current1
+    }
+    if(current2 !== null) {
+        tail.next = current2
+    }
+    
+    return dummy.next
+};
